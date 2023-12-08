f04bdcd4b44efa05a1bd057658910ed59039e13e
Antimundo <pablo.rubio.ru@gmail.com>
2023-12-08 19:42:38 +0100

    Update bullet_hole to use decal instead of a mesh

--- scenes/game_world/bullet_hole.gd
+++ scenes/game_world/bullet_hole.gd
@@ -1,4 +1,7 @@
 extends Node3D
 
-func _on_timer_timeout():
+func _ready():
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "modulate:a", 0, 20)
+	await tween.finished
 	queue_free()
