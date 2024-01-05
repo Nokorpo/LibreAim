edc1eb9cb93395ecdbecf344f35f8bc76306cd74
Antimundo <pablo.rubio.ru@gmail.com>
2024-01-05 15:53:07 +0100

    Add hit sound customization

--- scenes/game_world/destroyed_sound.gd
+++ scenes/game_world/destroyed_sound.gd
@@ -6,3 +6,10 @@ func _ready():
 		var this_bus = AudioServer.get_bus_index("Master")
 		AudioServer.set_bus_volume_db(this_bus, lerpf(-20, 0, volume))
 		AudioServer.set_bus_mute(this_bus, volume == 0)
+	update_hit_sound()
+
+func update_hit_sound():
+	var category = DataManager.categories.SETTINGS
+	if DataManager.get_data(category, "hit_sound") != null:
+		var selected = DataManager.get_data(category, "hit_sound")
+		stream = load(Audio.hit_sounds[selected])
--- scenes/main_menu/audio.gd
+++ scenes/main_menu/audio.gd
@@ -1,19 +1,40 @@
 extends Control
+class_name Audio
+
+const hit_sounds: Dictionary = {
+	"beep": "res://scenes/enemies/beep.ogg",
+	"bell": "res://scenes/enemies/bell.ogg",
+	"kalimba": "res://scenes/enemies/kalimba.ogg",
+}
 
 @onready var volume_label = $MarginContainer/VBoxContainer/VolumeLabel
 @onready var volume_slider = $MarginContainer/VBoxContainer/VolumeSlider
+@onready var option_button = $MarginContainer/VBoxContainer/OptionButton
+var category = DataManager.categories.SETTINGS
 
 func _ready():
-	var category = DataManager.categories.SETTINGS
+	category = DataManager.categories.SETTINGS
 	if DataManager.get_data(category, "volume") != null:
 		set_volume_label()
+	for sound in hit_sounds:
+		option_button.add_item(sound)
+	if DataManager.get_data(category, "hit_sound") != null:
+		var selected = DataManager.get_data(category, "hit_sound")
+		for i in range(option_button.item_count):
+			if selected == option_button.get_item_text(i):
+				option_button.select(i) 
 
 func _on_volume_slider_value_changed(value):
-	var category = DataManager.categories.SETTINGS
 	DataManager.save_data("volume", value, category)
 	set_volume_label()
 
 func set_volume_label():
-	var category = DataManager.categories.SETTINGS
 	volume_label.text = "Volume " + str(DataManager.get_data(category, "volume"))
 	volume_slider.value = DataManager.get_data(category, "volume")
+
+func _on_option_button_item_selected(index):
+	DataManager.save_data("hit_sound", hit_sounds.keys()[index], category)
+	$MarginContainer/VBoxContainer/Preview/AudioStreamPlayer.update_hit_sound()
+
+func _on_preview_pressed():
+	$MarginContainer/VBoxContainer/Preview/AudioStreamPlayer.play()
