56e2a721be7639fbc14d161e82edc1141d9a70d2
antimundo <pablo.rubio.ru@gmail.com>
2024-03-26 11:51:56 +0100

    Add target color customization

--- scenes/game_world/target.gd
+++ scenes/game_world/target.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 signal destroyed
 
 @onready var mesh := $CollisionShape3D/MeshInstance3D
+var color: Color
 
 var current_velocity = null
 var health = 0.0:
@@ -15,10 +16,10 @@ var health = 0.0:
 func _ready():
 	health = Global.current_gamemode.health
 	var category = DataManager.categories.SETTINGS
-	if DataManager.get_data(category, "TargetColor") != null:
+	if DataManager.get_data(category, "target_color") != null:
 		var material_override = mesh.get_mesh().get_material()
-		material_override.set_albedo(Global.string_to_color(DataManager.get_data(category, "TargetColor")))
-		material_override.set_emission(Global.string_to_color(DataManager.get_data(category, "TargetColor")))
+		material_override.set_albedo(Global.string_to_color(DataManager.get_data(category, "target_color")))
+		material_override.set_emission(Global.string_to_color(DataManager.get_data(category, "target_color")))
 		mesh.material_override = material_override
 
 func init(size = {"radius": .5, "height": 1}, movement = {"x": 0, "y": 0}):
--- scenes/main_menu/settings_world_appareance.gd
+++ scenes/main_menu/settings_world_appareance.gd
@@ -4,10 +4,16 @@ const CATEGORY = DataManager.categories.SETTINGS
 
 @onready var world_color = $WorldColor/Color
 @onready var world_texture = $WorldTexture
+@onready var target_color = $TargetColor/TargetColor
 @onready var preview = $Preview
+@onready var target_preview = $Preview/TargetPreview
 
-func _on_color_color_changed(color) -> void:
+func _on_color_color_changed(color: Color) -> void:
 	DataManager.save_data("world_color", str(color), CATEGORY)
+	update_preview()
+
+func _on_target_color_color_changed(color: Color) -> void:
+	DataManager.save_data("target_color", str(color), CATEGORY)
 	update_preview()
 
 func _on_world_texture_item_selected(index) -> void:
@@ -20,9 +26,11 @@ func _ready() -> void:
 		world_texture.add_icon_item(load(Global.get_world_textures_path() + texture), texture, i)
 		i+=1
 	world_color.color = DataManager.set_color_if_exists(CATEGORY, world_color.color, "world_color")
+	target_color.color = DataManager.set_color_if_exists(CATEGORY, target_color.color, "target_color")
 	get_selected_texture_index()
+	update_preview()
 
-func get_selected_texture_index():
+func get_selected_texture_index() -> void:
 	if DataManager.get_data(CATEGORY, "world_texture"):
 		var selected = DataManager.get_data(CATEGORY, "world_texture")
 		for i in range(world_texture.item_count):
@@ -32,4 +40,5 @@ func get_selected_texture_index():
 
 func update_preview() -> void:
 	preview.texture = Global.get_current_world_texture()
-	preview.modulate = DataManager.set_color_if_exists(CATEGORY, preview.modulate, "world_color")
+	preview.self_modulate = DataManager.set_color_if_exists(CATEGORY, preview.self_modulate, "world_color")
+	target_preview.color = DataManager.set_color_if_exists(CATEGORY, target_preview.color, "target_color")
