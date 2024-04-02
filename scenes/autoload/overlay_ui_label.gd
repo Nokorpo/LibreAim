4c84bdf9d83c27f6e6cc125d5a07becd91fd4e34
Djkcool <djkprivate18@gmail.com>
2024-04-02 11:42:21 +0200

    Added a FPS overlay toggle
    Added a toggle that enables and disables the FPS overlay.

--- scenes/autoload/overlay_ui_label.gd
+++ scenes/autoload/overlay_ui_label.gd
@@ -1,5 +1,8 @@
 extends Label
 
 func _process(_delta: float) -> void:
-	set_text("FPS " + str(Engine.get_frames_per_second()))
+	if DataManager.get_data(DataManager.categories.SETTINGS, "fps_overlay"):
+		set_text("FPS " + str(Engine.get_frames_per_second()))
+	else:
+		set_text("")
 
