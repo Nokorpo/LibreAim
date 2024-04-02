0e77d684bd620f8c03094cc38e5654f46f351bea
Djkcool <djkprivate18@gmail.com>
2024-04-02 17:12:57 +0200

    Added number of missed shots
    Made it so that at the end of a game it displays how many shots the player missed during the game.

--- scenes/game_world/end_game_canvas.gd
+++ scenes/game_world/end_game_canvas.gd
@@ -1,13 +1,14 @@
 extends Control
 ## Canvas shown when game ends
 
-func set_score(score: int, high_score: int) -> void:
+func set_score(score: int, high_score: int, missed_shots: int) -> void:
 	$Button.grab_focus()
 	var text = "Score: " + str(score)
 	if score > high_score:
 		text += "\nNew high score!"
 	else:
 		text += "\nCurrent high score: " + str(high_score)
+	text += "\nShots missed: " + str(missed_shots)
 	$Score.text = text
 
 func _on_button_pressed():
