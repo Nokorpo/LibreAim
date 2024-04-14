04bc5c4e076a9e4707b0267b4892375bc27776e0
antimundo <pablo.rubio.ru@gmail.com>
2024-04-14 15:41:10 +0200

    Add slider to sensitivity settings

--- scenes/main_menu/settings/settings_controls.gd
+++ scenes/main_menu/settings/settings_controls.gd
@@ -8,24 +8,24 @@ const games_sensitivities: Dictionary = {
 	"Valorant": 0.07,
 }
 
-@onready var game = $HBoxContainer2/Game
-@onready var sensitivity = $HBoxContainer2/Sensitivity
+@onready var game = $Game
+@onready var sensitivity = $Sensitivity
 
 func _ready() -> void:
 	for sens in games_sensitivities:
 		game.add_item(sens)
 	var category = DataManager.categories.SETTINGS
 	if DataManager.get_data(category, "sensitivity"):
-		sensitivity.text = str(DataManager.get_data(category, "sensitivity"))
+		sensitivity.value = DataManager.get_data(category, "sensitivity")
 	if DataManager.get_data(category, "sensitivity_game"):
 		game.selected = DataManager.get_data(category, "sensitivity_game")
 
-func _on_sensitivity_text_changed(new_text: String) -> void:
+func _on_sensitivity_change_value(value) -> void:
 	DataManager.save_data("sensitivity_game", game.get_selected_id(), \
 		DataManager.categories.SETTINGS)
 	DataManager.save_data("sensitivity_game_value", games_sensitivities.get(game.get_item_text(game.get_selected_id())), \
 		DataManager.categories.SETTINGS)
-	DataManager.save_data("sensitivity", float(new_text), \
+	DataManager.save_data("sensitivity", float(value), \
 		DataManager.categories.SETTINGS)
 
 func _on_game_item_selected(index: int) -> void:
