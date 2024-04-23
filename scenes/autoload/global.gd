2f5e22601082316685d45f6b72379874ecad7a48
antimundo <pablo.rubio.ru@gmail.com>
2024-04-23 09:53:09 +0200

    Add option to create custom gamemodes
    This closes #20

--- scenes/autoload/global.gd
+++ scenes/autoload/global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const TEXTURES_FOLDER := "world_textures"
+const GAMEMODES_FOLDER := "gamemodes"
 
 var gamemodes : Dictionary
 var current_gamemode : Dictionary
@@ -10,37 +11,28 @@ func _ready() -> void:
 	load_gamemodes()
 
 func load_gamemodes() -> void:
-	const PATH = "res://assets/gamemodes/"
-	
-	var dir = DirAccess.open(PATH)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if dir.current_is_dir():
-				pass
-			else:
-				var config = ConfigFile.new()
-				config.load(PATH + file_name)
-				var id = config.get_value("metadata", "id")
-				var this_gamemode : Dictionary = { }
-				this_gamemode.id = id
-				this_gamemode.title = config.get_value("metadata", "title")
-				this_gamemode.description = config.get_value("metadata", "description")
-				this_gamemode.time = config.get_value("settings", "time")
-				this_gamemode.player_movement = config.get_value("settings", "player_movement")
-				this_gamemode.movement = config.get_value("target", "movement")
-				this_gamemode.health = config.get_value("target", "health")
-				this_gamemode.size = config.get_value("target", "size")
-				this_gamemode.initial_targets = config.get_value("target", "initial_targets")
-				this_gamemode.spawn_location = config.get_value("target", "spawn_location")
-				gamemodes[id] = this_gamemode
-			file_name = dir.get_next()
-	else:
-		push_warning("An error occurred when trying to access the path.")
+	for path: String in Global.get_gamemodes():
+		var config = ConfigFile.new()
+		config.load(path)
+		var id = config.get_value("metadata", "id")
+		var this_gamemode : Dictionary = { }
+		this_gamemode.id = id
+		this_gamemode.title = config.get_value("metadata", "title")
+		this_gamemode.description = config.get_value("metadata", "description")
+		this_gamemode.time = config.get_value("settings", "time")
+		this_gamemode.player_movement = config.get_value("settings", "player_movement")
+		this_gamemode.movement = config.get_value("target", "movement")
+		this_gamemode.health = config.get_value("target", "health")
+		this_gamemode.size = config.get_value("target", "size")
+		this_gamemode.initial_targets = config.get_value("target", "initial_targets")
+		this_gamemode.spawn_location = config.get_value("target", "spawn_location")
+		gamemodes[id] = this_gamemode
 
 func get_world_textures() -> PackedStringArray:
 	return CustomResourceManager.get_file_list(TEXTURES_FOLDER, "png")
+
+func get_gamemodes() -> PackedStringArray:
+	return CustomResourceManager.get_file_list(GAMEMODES_FOLDER, "cfg")
 
 func get_current_world_texture() -> Texture2D:
 	var wrapper := DataManager.get_wrapper(DataManager.SETTINGS_FILE_PATH, "world")
