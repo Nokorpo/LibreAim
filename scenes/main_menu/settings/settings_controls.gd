5b27d4f22de7559aae3d2599c35256791aff7a4b
antimundo <pablo.rubio.ru@gmail.com>
2024-08-17 20:50:12 +0200

    Fix settings setting a wrong sensitivity_game

--- scenes/main_menu/settings/settings_controls.gd
+++ scenes/main_menu/settings/settings_controls.gd
@@ -20,7 +20,6 @@ func _ready() -> void:
 	for sens in games_sensitivities:
 		game.add_item(sens)
 	sensitivity.value = data_wrapper.get_data("sensitivity")
-	#game.selected = data_wrapper.get_data("sensitivity_game")
 	var selected = data_wrapper.get_data("sensitivity_game")
 	for i in range(game.item_count):
 		if selected == game.get_item_text(i):
@@ -29,10 +28,9 @@ func _ready() -> void:
 	game.item_selected.connect(_on_game_item_selected)
 
 func _on_sensitivity_change_value(value) -> void:
-	data_wrapper.set_data("sensitivity_game", game.get_selected_id(), )
+	data_wrapper.set_data("sensitivity_game", game.get_item_text(game.get_item_index(game.get_selected_id())))
 	data_wrapper.set_data("sensitivity_game_value", games_sensitivities.get(game.get_item_text(game.get_selected_id())) )
 	data_wrapper.set_data("sensitivity", float(value))
-	print(data_wrapper.get_data("sensitivity"))
 
 func _on_game_item_selected(index: int) -> void:
 	data_wrapper.set_data("sensitivity_game", game.get_item_text(index))
