b3fc6b8245c1a6212657d867861fa2e3371a0919
antimundo <64472605+antimundo@users.noreply.github.com>
2025-02-07 00:47:50 +0100

    Refactor global.gd
    Now gamemodes load in a more logical way

--- scenes/autoload/global.gd
+++ scenes/autoload/global.gd
@@ -9,27 +9,7 @@ var current_gamemode : Dictionary
 
 func _ready() -> void:
 	Input.set_use_accumulated_input(false)
-	load_gamemodes()
-
-func load_gamemodes() -> void:
-	for path: String in Global.get_gamemodes():
-		var config = ConfigFile.new()
-		config.load(path)
-		var id = config.get_value("metadata", "id")
-		var this_gamemode : Dictionary = { }
-		this_gamemode.id = id
-		this_gamemode.title = config.get_value("metadata", "title")
-		this_gamemode.description = config.get_value("metadata", "description")
-		this_gamemode.time = config.get_value("settings", "time")
-		this_gamemode.player_movement = config.get_value("settings", "player_movement")
-		this_gamemode.score_per_hit = config.get_value("score", "score_per_hit")
-		this_gamemode.accuracy_multiplier = config.get_value("score", "accuracy_multiplier")
-		this_gamemode.movement = config.get_value("target", "movement")
-		this_gamemode.health = config.get_value("target", "health")
-		this_gamemode.size = config.get_value("target", "size")
-		this_gamemode.initial_targets = config.get_value("target", "initial_targets")
-		this_gamemode.spawn_location = config.get_value("target", "spawn_location")
-		gamemodes[id] = this_gamemode
+	_load_gamemodes()
 
 func get_current_gamemode_value(value: String) -> Variant:
 	if Global.current_gamemode.is_empty():
@@ -71,3 +51,16 @@ func get_current_hit_sound() -> AudioStream:
 		wrapper.set_data("hit_sound", current_sound)
 	var sound := CustomResourceManager.get_sound(current_sound)
 	return sound
+
+func _load_gamemodes() -> void:
+	for path: String in Global.get_gamemodes():
+		var config = ConfigFile.new()
+		config.load(path)
+		var id = config.get_value("metadata", "id")
+		var this_gamemode : Dictionary = { }
+		
+		this_gamemode.id = id
+		for section: String in config.get_sections():
+			for key: String in config.get_section_keys(section):
+				this_gamemode[key] = config.get_value(section, key)
+		gamemodes[id] = this_gamemode
