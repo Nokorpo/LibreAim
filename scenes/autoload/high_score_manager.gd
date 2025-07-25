acce2483411d605198d262b1769c20bf2aaf3e94
Antimundo <pablo.rubio.ru@gmail.com>
2025-07-25 15:06:10 +0200

    Fix progress not saving if folder not exists

--- scenes/autoload/high_score_manager.gd
+++ scenes/autoload/high_score_manager.gd
@@ -1,6 +1,8 @@
 class_name HighScoreManager
 ## Manages the high scores saved by the user
 
+const PROGRESS_PATH = "user://data/progress/"
+
 static func save_high_score(key: String, value) -> void:
 	if is_high_score(key, value):
 		SaveManager.highscores.set_data("user", key, value)
@@ -12,6 +14,7 @@ static func save_high_score(key: String, value) -> void:
 	var cfg := ConfigFile.new()
 	cfg.load(cfg_path)
 	cfg.set_value(Time.get_date_string_from_system(), time_string, value)
+	create_folder_if_not_exists(PROGRESS_PATH)
 	cfg.save(cfg_path)
 
 static func is_high_score(key: String, value) -> bool:
@@ -22,3 +25,9 @@ static func get_high_score(key: String) -> int:
 
 static func get_progress_path(scenario_id: String) -> String:
 	return "user://data/progress/" + scenario_id + ".cfg"
+
+static func create_folder_if_not_exists(folder_path: String) -> void:
+	if not DirAccess.dir_exists_absolute(folder_path):
+		var err = DirAccess.make_dir_recursive_absolute(folder_path)
+		if err != OK:
+			push_error("Failed to create directory: %s (Error code: %d)" % [folder_path, err])
